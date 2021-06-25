9e517eb6f334dfcbf04df65e0d4e71529989d838
TrainerHol <79064972+AndresIPT2021@users.noreply.github.com>
2021-06-25 08:41:25 -0600

    SURF.Finance Interface

--- contracts/GameDevCredits.sol
+++ contracts/GameDevCredits.sol
@@ -7,6 +7,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+interface SURF {
+    function balanceOf(address) external view returns (uint256);
+    function transfer(address, uint256) external returns (bool);
+    function transferFrom(address, address, uint256) external returns (bool);
+}
+
 contract GameDevCredits is
     ERC721,
     ERC721Enumerable,
