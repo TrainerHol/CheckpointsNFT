3a901ee0fc4321104ac1756d40d2ed164f0990e5
TrainerHol <79064972+AndresIPT2021@users.noreply.github.com>
2021-06-30 00:52:25 -0600

    Property access control and minting

--- contracts/CreditState.sol
+++ contracts/CreditState.sol
@@ -1,54 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CreditState {
+contract Credit is Ownable {
     mapping(uint256 => CreditStringProperty) stringProperties;
     uint256 public stringIndex;
     mapping(uint256 => CreditNumberProperty) numberProperties;
     uint256 public intIndex;
 
     mapping(uint256 => CreditProposal) proposals;
-    mapping(uint256 => Credit) createdCredits;
+    mapping(uint256 => CreditFields) createdCredits;
 
     enum FlowState {
         OPEN,
         ACCEPTED,
         DENIED
     }
-    struct Credit {
+    struct CreditFields {
         mapping(uint256 => string) stringProperties;
         mapping(uint256 => uint256) numberProperties;
     }
     struct CreditProposal {
         FlowState flowState;
-        Credit credit;
+        CreditFields credit;
     }
     struct CreditStringProperty {
         string propertyName;
-        bytes1 editable;
+        bool editable;
     }
     struct CreditNumberProperty {
         string propertyName;
-        bytes1 editable;
-    }
-
-    function EditCreditString(
-        uint256 _creditId,
-        uint256 _propertyIndex,
-        uint256 _propertyValue
-    ) public {
-        // TODO: Check that the value is editable
-    }
-
-    function AddNumberProperty(string memory _name, bool _editable) public {
-        bytes1 editable = _editable ? (bytes1((uint8(1)))) : (bytes1(0));
-        numberProperties[intIndex] = CreditNumberProperty(_name, editable);
-        intIndex++;
-    }
-
-    function AddStringProperty(string memory _name, bool _editable) public {
-        bytes1 editable = _editable ? (bytes1((uint8(1)))) : (bytes1(0));
-        stringProperties[stringIndex] = CreditStringProperty(_name, editable);
-        stringIndex++;
+        bool editable;
     }
 }
--- contracts/GameDevCredits.sol
+++ contracts/GameDevCredits.sol
@@ -6,32 +6,51 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./CreditState.sol";
 
 interface SURF {
     function balanceOf(address) external view returns (uint256);
+
     function transfer(address, uint256) external returns (bool);
-    function transferFrom(address, address, uint256) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
 }
 
 contract GameDevCredits is
     ERC721,
     ERC721Enumerable,
     ERC721URIStorage,
-    AccessControl
+    AccessControl,
+    Credit
 {
     using Counters for Counters.Counter;
 
     bytes32 public constant MINTER_ROLE = keccak256("GAME_DEV");
+    uint256 maxSupply;
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("Checkpoints", "CHECKPOINTS") {
+    constructor(uint256 _maxSupply) ERC721("Checkpoints", "CHECKPOINTS") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
+        maxSupply = _maxSupply;
     }
 
-    function safeMint(address to) public {
+    function safeMint(address to, uint256 proposalId) public {
         require(hasRole(MINTER_ROLE, msg.sender));
+        require(
+            proposals[proposalId].flowState == FlowState.ACCEPTED,
+            "Proposal has not been accepted"
+        );
+        require(_tokenIdCounter.current() < maxSupply, "Max supply reached");
         _safeMint(to, _tokenIdCounter.current());
+        CreditFields storage credits = createdCredits[
+            _tokenIdCounter.current()
+        ];
+        credits = proposals[proposalId].credit;
         _tokenIdCounter.increment();
     }
 
@@ -66,5 +85,59 @@ contract GameDevCredits is
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    modifier onlyTokenOwner(uint256 _tokenId) {
+        require(ownerOf(_tokenId) == msg.sender, "");
+        _;
+    }
+
+    function CreateCredit(
+        uint256[] memory numberValues,
+        string[] memory stringValues
+    ) public onlyOwner {}
+
+    function EditCreditString(
+        uint256 _creditId,
+        uint256 _propertyIndex,
+        string memory _propertyValue
+    ) public onlyTokenOwner(_creditId) {
+        require(
+            stringProperties[_propertyIndex].editable,
+            "Property not editable"
+        );
+        createdCredits[_creditId].stringProperties[
+            _propertyIndex
+        ] = _propertyValue;
+    }
+
+    function EditCreditNumber(
+        uint256 _creditId,
+        uint256 _propertyIndex,
+        uint256 _propertyValue
+    ) public onlyTokenOwner(_creditId) {
+        require(
+            numberProperties[_propertyIndex].editable,
+            "Property not editable"
+        );
+        createdCredits[_creditId].numberProperties[
+            _propertyIndex
+        ] = _propertyValue;
+    }
+
+    function AddNumberProperty(string memory _name, bool _editable)
+        public
+        onlyOwner
+    {
+        numberProperties[intIndex] = CreditNumberProperty(_name, _editable);
+        intIndex++;
+    }
+
+    function AddStringProperty(string memory _name, bool _editable)
+        public
+        onlyOwner
+    {
+        stringProperties[stringIndex] = CreditStringProperty(_name, _editable);
+        stringIndex++;
     }
 }
