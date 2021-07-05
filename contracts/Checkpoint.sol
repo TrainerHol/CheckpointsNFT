77b78dfb5f7ea6b6eb1cc94be1abd8598a2e2073
TrainerHol <79064972+AndresIPT2021@users.noreply.github.com>
2021-07-04 20:39:38 -0600

    Fix integer value concatenation

--- contracts/Checkpoint.sol
+++ contracts/Checkpoint.sol
@@ -129,7 +129,10 @@ contract Checkpoint is
             metadata = string(abi.encodePacked(metadata, '": '));
             metadata = string(abi.encodePacked(metadata, '"'));
             metadata = string(
-                abi.encodePacked(metadata, fields.numberProperties[index])
+                abi.encodePacked(
+                    metadata,
+                    uint2str(fields.numberProperties[index])
+                )
             );
             metadata = string(abi.encodePacked(metadata, '"'));
             if (index != intIndex - 1) {
@@ -251,5 +254,31 @@ contract Checkpoint is
     modifier onlyWhenLocked() {
         require(isLocked, "Structure must be locked first");
         _;
+    }
+
+    function uint2str(uint256 _i)
+        internal
+        pure
+        returns (string memory _uintAsString)
+    {
+        if (_i == 0) {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len;
+        while (_i != 0) {
+            k = k - 1;
+            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
     }
 }
